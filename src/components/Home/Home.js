de2253dadbe6c2de6ba1b473efaa6b3b4e7648bf
Alexander Bos <abos@revridge.com>
2021-01-07 19:41:56 -0500

    1/6 change on home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Home (props) {
   return (
     <div className="container">
-      <h1 id="title"></h1>
-      <img src="https://i.imgur.com/jwdIZD8.jpg" className="img-fluid w-50" id="aboutpic" alt="profile pic" ></img>
+      <img src="https://i.imgur.com/jwdIZD8.jpg" className="img-fluid w-50 p-3" id="aboutpic" alt="profile pic" ></img>
       <br/>
+      <h3 id="title">Alexander Bos</h3>
       <div className="shadow p-3 mb-5 bg-white w-auto rounded">
         <p> Digitally aged full stack software engineer convert with exceptional analytical and problem-solving skills. A former real estate professional with a decade of experience, highly accomplished in a team setting. Offering entrepreneurial adaptability and heightened attention to detail.
         </p>
